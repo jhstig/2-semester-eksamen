60dff621bbf9b6cb766ee06e5b0e4afd02bebd1c
Merve <mervekync98@hotmail.com>
2021-06-06 16:43:52 +0200

    Min profil

--- profile.php
+++ profile.php
@@ -1,9 +1,52 @@
-<?php include("templates/header.php");
-session_start();?>
+<?php
+
+session_start();
+
+include("templates/header.php");
+include("templates/profile-header.php");
+ ?>
+
+<div class="container">
+    <div class="row">
+      <div class="col">
+        Profil
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        Fornavn
+        Efternavn
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        Email
+      </div>
+    </div>
+      <div class="row">
+        <div class="col">
+          Kodeord
+      </div>
+    </div>
+
+
+
+
+      </div>
+
+      </div>
+
+    </div>
 
 
 
 
 
 
-<?php include("templates/footer.php"); ?>
+
+</div>
+
+<?php
+include("templates/footer.php");
+
+ ?>
---
+++ templates/profile-header.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row justify-content-center my-4">
+      <div class="col-3 box-me-custom text-center mx-4">
+        <a href="profile.php">Min profil</a>
+      </div>
+      <div class="col-3 box-me-custom text-center mx-4">
+        <a href="my-auctions.php">Mine auktioner</a>
+      </div>
+      <div class="col-3 box-me-custom text-center mx-4">
+        <a href="create.php">Opret auktion</a>
+      </div>
+    </div>
+
+</div>
