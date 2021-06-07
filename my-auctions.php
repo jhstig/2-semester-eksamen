67b599ce3452a6e328b121926889e92db3e528bc
Merve <mervekync98@hotmail.com>
2021-06-07 17:59:50 +0200

    my-auctions - won

--- my-auctions.php
+++ my-auctions.php
@@ -3,55 +3,119 @@ session_start();
 include("templates/header.php");
 include("templates/profile-header.php");
 ?>
+<div class="container-fluid">
+  <h5 class="text-center font-weight-bold bg-light">Mine auktioner</h5>
+    <div class="col mt-3">
 
-<div class="container pt-3">
+
+  </div>
+</div>
+  <div class="container">
     <div class="row justify-content-center">
-      <div class="col-md-6">
-        <h1> Mine auktioner</h1>
-          <div class="form-group">
-            <label for="Title">Title</label>
-          <div class="car col-md-3 col-12">
-            <div class="card-content">
-              <div class="card-body"><img class="img" src=
-                <div class="shadow"></div>
-                <div class="card-title"> Titel1</div>
-                <div class="card-subtitle">
-                  <p><small class="text-muted">Beskrivelse</small></p>
-                </div>
-              </div>
+      <div class="col-md-3 bg-light rounded">
+        <div class="row">
+          <div class="col mt-3 text-center">
+            <img src="img/placeholder.png" alt="" class="img-thumbnail">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col mt-3">
+            <span class="font-weight-bold h3">
+              Titel
+            </span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col mt-2">
+            <div class="font-weight-bold">
+            Beskrivelse
             </div>
           </div>
-          <div class="card col-md-3 col-12 ml-2">
-            <div class="card-content">
-              <div class="card-body"><img class="img" src=
-                <div class="card-title">Titel2</div>
-                <div class="card-subtitle">
-                  <p><small class="text-muted">Beskrivelse</small></p>
-                </div>
-              </div>
+        </div>
+        <div class="row">
+          <div class="col mt-2">
+            <div class="font-weight-bold">
+            Pris
             </div>
           </div>
-
-
-
-
-
-
-
-
-
-                </div>
-
+        </div>
+        <div class="row">
+          <div class="col my-2 ">
+            <div class="font-weight-bold">
+            Udløbsdato
             </div>
-
           </div>
-
-
-
-
+        </div>
 
     </div>
-</div>
+  </div>
+
+
+<hr>
+
+ <div class="container-fluid">
+   <h5 class="text-center font-weight-bold bg-warning">Vundne aktioner</h5>
+     <div class="col mt-3">
+
+
+     </div>
+    </div>
+  </div>
+
+  <div class="container bg-light pt-4 rounded">
+    <div class="row">
+      <div class="col-6">
+        <div class="font-weight-bold">
+        Senest vundne
+        </div>
+      </div>
+      <div class="col-2">
+        <div class="font-weight-bold">
+        Pris
+      </div>
+      </div>
+      <div class="col">
+        <div class="font-weight-bold">
+        Vundet/Budt
+      </div>
+      </div>
+      <div class="col-2">
+        <div class="font-weight-bold">
+        Lukket tidspunkt
+      </div>
+      </div>
+      <div class="col-2">
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-2">
+        Billede
+      </div>
+      <div class="col-4">
+        <div class="row">
+          <div class="col">
+            Titel
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            Beskrivelse
+          </div>
+        </div>
+      </div>
+      <div class="col-2">
+        Pris
+      </div>
+      <div class="col-2">
+        Vundet/budt
+      </div>
+      <div class="col-2">
+        Dato
+      </div>
+    </div>
+  </div>
+
 
 
 
