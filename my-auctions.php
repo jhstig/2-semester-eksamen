ad2ee463425525fef428663ba277b215b3f7ef14
Merve <mervekync98@hotmail.com>
2021-06-07 14:44:34 +0200

    m

--- my-auctions.php
+++ my-auctions.php
@@ -4,6 +4,54 @@ include("templates/header.php");
 include("templates/profile-header.php");
 ?>
 
+<div class="container pt-3">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <h1> Mine auktioner</h1>
+          <div class="form-group">
+            <label for="Title">Title</label>
+          <div class="car col-md-3 col-12">
+            <div class="card-content">
+              <div class="card-body"><img class="img" src=
+                <div class="shadow"></div>
+                <div class="card-title"> Titel1</div>
+                <div class="card-subtitle">
+                  <p><small class="text-muted">Beskrivelse</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card col-md-3 col-12 ml-2">
+            <div class="card-content">
+              <div class="card-body"><img class="img" src=
+                <div class="card-title">Titel2</div>
+                <div class="card-subtitle">
+                  <p><small class="text-muted">Beskrivelse</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+
+
+
+
+                </div>
+
+            </div>
+
+          </div>
+
+
+
+
+
+    </div>
+</div>
 
 
 
